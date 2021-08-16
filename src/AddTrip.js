c84384b69b864ed3a20941b38b9cd9a45c819853
Sam <shar0016@student.monash.edu>
2021-08-17 01:07:35 +1000

    added ability to add new location to checklist (no geocoding yet)

--- src/AddTrip.js
+++ src/AddTrip.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
-import { Button, Stepper, Step, StepLabel, Typography } from "@material-ui/core";
+import { Button, Stepper, Step, StepLabel, Grid, TextField } from "@material-ui/core";
 import { DateTimePicker } from "@material-ui/pickers";
-import Trips from './Trips';
-import CheckboxList from './components/CheckboxList.js'
-import DragDropList from './components/DragDropList.js'
-import Map from './components/Map.js'
-import { getExampleCars, getExampleLocations } from './Classes';
 import { makeStyles } from '@material-ui/core/styles';
+import CheckboxList from './components/CheckboxList.js'
+import Map from './components/Map.js'
+import Trips from './Trips';
+import { getExampleCars, getExampleLocations, Location } from './Classes';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,12 +23,10 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function AddTrip(props) {
+  // whole page constants
   const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
   const steps = ['Select Waypoints', 'Assign Vehicles', 'Extra Details'];
-  const [selectedDate, handleDateChange] = React.useState(new Date());
-  const cars = getExampleCars();
-  const locations = getExampleLocations();
+  const [activeStep, setActiveStep] = React.useState(0);
 
   const handleNext = () => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
@@ -43,18 +41,30 @@ export default function AddTrip(props) {
       case 0: // Waypoints
         return (
           <div>
-            <div>
-              <DragDropList initialList={locations} />
-            </div>
-            <Map height="400px" />
-          </div>
+            <Grid container spacing={2}>
+              <Grid item xs>
+                <div>
+                  <TextField id="geocoder-input" label="Search Address Here" onChange={(e) => setGeocoderInput(e.target.value)} />
+                  <Button variant="contained" color="secondary">Search</Button>
+                  <TextField id="location-nickname-input" label="Nickname for Location" onChange={(e) => setlocNickname(e.target.value)} />
+                  <Button variant="contained" color="secondary" onClick={() => setLocations([new Location(0, 0, locNickname), ...locations])}>Add</Button>
+
+                  <CheckboxList items={locations} handleToggle={handleToggle(setLocsChecked)} checked={locsChecked} />
+                </div>
+                {/* <DragDropList initialList={locations} /> */}
+              </Grid>
+              <Grid item xs>
+                <Map height="700px" />
+              </Grid>
+            </Grid>
+          </div >
 
         );
 
       case 1: // Vehicles
         return (
           <div className="row">
-            <CheckboxList items={cars} />
+            <CheckboxList items={cars} handleToggle={handleToggle(setVehiChecked)} checked={vehiChecked} />
           </div>
         );
 
@@ -80,41 +90,77 @@ export default function AddTrip(props) {
     }
   };
 
+  //waypoint page constants
+  const [geocoderInput, setGeocoderInput] = React.useState('');
+  const [locNickname, setlocNickname] = React.useState('');
+  const [locsChecked, setLocsChecked] = React.useState([]);
+  const [locations, setLocations] = React.useState(getExampleLocations());
+  const [vehiChecked, setVehiChecked] = React.useState([]);
+
+  const handleToggle = (updater) => (checked) => (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    updater(newChecked);
+  };
+
+  //vehicle page constants
+  const cars = getExampleCars();
+
+  //details page constants
+  const [selectedDate, handleDateChange] = React.useState(new Date());
+
   return (
     <div className={classes.root}>
-      <h2>New Trip</h2>
-
-      <div className={`classes.instructions divBox row`} style={{ width: "100%" }}>{getStepContent(activeStep)}</div>
-
-      <div className="divBox row" style={{ display: "inline" }}>
-        <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
-          Back
-        </Button>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={activeStep === steps.length - 1 ?
-            () => props.pageUpdater(<Trips pageUpdater={props.pageUpdater} addToDatabase={props.addToDatabase} user={props.user} />) // TODO: Add to database instead of just returning to trips page
-            : handleNext}
-          className={classes.button}
-        >
-          {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-        </Button>
+      <div className="row">
+        <div className="divBox">
+          <h2>New Trip</h2>
+        </div>
       </div>
 
-      <div className="divBox row" style={{ width: "100%" }}>
-        <Stepper activeStep={activeStep}>
-          {steps.map((label, index) => {
-            const stepProps = {};
-            const labelProps = {};
-            return (
-              <Step key={label} {...stepProps}>
-                <StepLabel {...labelProps}>{label}</StepLabel>
-              </Step>
-            );
-          })}
-        </Stepper>
+      <div className="row">
+        <div className={`classes.instructions divBox`} style={{ width: "100%" }}>{getStepContent(activeStep)}</div>
+      </div>
+
+      <div className="row">
+        <div className="divBox" style={{ display: "inline" }}>
+          <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+            Back
+          </Button>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={activeStep === steps.length - 1 ?
+              () => props.pageUpdater(<Trips pageUpdater={props.pageUpdater} addToDatabase={props.addToDatabase} user={props.user} />) // TODO: Add to database instead of just returning to trips page
+              : handleNext}
+            className={classes.button}
+          >
+            {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+          </Button>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="divBox" style={{ width: "100%" }}>
+          <Stepper activeStep={activeStep}>
+            {steps.map((label, index) => {
+              const stepProps = {};
+              const labelProps = {};
+              return (
+                <Step key={label} {...stepProps}>
+                  <StepLabel {...labelProps}>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
+        </div>
       </div>
     </div>
   );
--- src/Classes.js
+++ src/Classes.js
@@ -1,94 +1,95 @@
 export class Vehicle {
-    constructor(make, model, year, autoTransmission, avgEmissionsPerKm, rego) {
-      this.make = make;
-      this.model = model;
-      this.year = year;
-      this.autoTransmission = autoTransmission;
-      this.avgEmissionsPerKm = avgEmissionsPerKm;
-      this.rego = rego;
-    }
-
-    toString(){
-      return this.rego + " - " + this.make + " " + this.model + " " + this.year;
-    }
-
-    objectToInstance(obj, dbKey) {
-      this.dbKey = dbKey;
-      this.make = obj.make;
-      this.model = obj.model;
-      this.year = obj.year;
-      this.autoTransmission = obj.autoTransmission;
-      this.avgEmissionsPerKm = obj.avgEmissionsPerKm;
-      this.rego = obj.rego;
-    }
-  };
-  
-  class Location {
-    constructor(lat, long, nickname) {
-      this.lat = lat;
-      this.long = long;
-      this.nickname = nickname;
-    }
-
-    toString(){
-      return this.nickname;
-    }
+  constructor(make, model, year, autoTransmission, avgEmissionsPerKm, rego) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.autoTransmission = autoTransmission;
+    this.avgEmissionsPerKm = avgEmissionsPerKm;
+    this.rego = rego;
   }
-  
-  class TripLeg {
-    constructor(startLocation, endLocation, duration, distance, vehicle) {
-      this.startLocation = startLocation;
-      this.endLocation = endLocation;
-      this.duration = duration;
-      this.distance = distance;
-      this.vehicle = vehicle;
-      this.legEmissions = vehicle.avgEmissionsPerKm * distance;
-    }
-  };
 
-  class VehicleTrip {
-    constructor(vehicle, tripLegs) {
-      this.vehicle = vehicle;
-      this.tripLegs = tripLegs;
-      [this.vehicleDistance, this.vehicleDuration] = tripLegs.reduce(
-        (accumulator, currentTripLeg) => 
+  toString() {
+    return this.rego + " - " + this.make + " " + this.model + " " + this.year;
+  }
+
+  objectToInstance(obj, dbKey) {
+    this.dbKey = dbKey;
+    this.make = obj.make;
+    this.model = obj.model;
+    this.year = obj.year;
+    this.autoTransmission = obj.autoTransmission;
+    this.avgEmissionsPerKm = obj.avgEmissionsPerKm;
+    this.rego = obj.rego;
+  }
+};
+
+export class Location {
+  constructor(lat, long, nickname) {
+    this.lat = lat;
+    this.long = long;
+    this.nickname = nickname;
+  }
+
+  toString() {
+    return this.nickname;
+  }
+}
+
+class TripLeg {
+  constructor(startLocation, endLocation, duration, distance, vehicle) {
+    this.startLocation = startLocation;
+    this.endLocation = endLocation;
+    this.duration = duration;
+    this.distance = distance;
+    this.vehicle = vehicle;
+    this.legEmissions = vehicle.avgEmissionsPerKm * distance;
+  }
+};
+
+class VehicleTrip {
+  constructor(vehicle, tripLegs) {
+    this.vehicle = vehicle;
+    this.tripLegs = tripLegs;
+    [this.vehicleDistance, this.vehicleDuration] = tripLegs.reduce(
+      (accumulator, currentTripLeg) =>
         [accumulator[0] + currentTripLeg.distance, accumulator[1] + currentTripLeg.duration], [0, 0]
-        );
-      this.vehicleEmissions = vehicle.avgEmissionsPerKm * this.vehicleDistance;
-    }
+    );
+    this.vehicleEmissions = vehicle.avgEmissionsPerKm * this.vehicleDistance;
   }
-  
-  export class Trip {
-      constructor(date, vehicleTrips) {
-        this.date = date;
-        this.vehicleTrips = vehicleTrips;
-        if (vehicleTrips !== undefined){
-        [this.distance, this.totalDuration, this.emissions, this.consecutiveDuration] = vehicleTrips.reduce(
-          (accumulator, currentVehicleTrip) =>
+}
+
+export class Trip {
+  constructor(date, vehicleTrips) {
+    this.date = date;
+    this.vehicleTrips = vehicleTrips;
+    if (vehicleTrips !== undefined) {
+      [this.distance, this.totalDuration, this.emissions, this.consecutiveDuration] = vehicleTrips.reduce(
+        (accumulator, currentVehicleTrip) =>
           [
-            accumulator[0] + currentVehicleTrip.vehicleDistance, 
+            accumulator[0] + currentVehicleTrip.vehicleDistance,
             accumulator[1] + currentVehicleTrip.vehicleDuration,
             accumulator[2] + currentVehicleTrip.vehicleEmissions,
             Math.max(accumulator[3], currentVehicleTrip.vehicleDuration)
-          ], 
-          [0, 0, 0, 0]
-        );}
-      }
-      objectToInstance(obj, dbKey) {
-        this.dbKey = dbKey;
-        this.consecutiveDuration = obj.consecutiveDuration;
-        this.date = new Date(Date.parse(obj.date));
-        this.distance = obj.distance;
-        this.emissions = obj.emissions;
-        this.totalDuration = obj.totalDuration;
-        this.vehicleTrips = obj.vehicleTrips.map((e) => new VehicleTrip(
-          new Vehicle(e.vehicle.make, e.vehicle.model, e.vehicle.year, e.vehicle.autoTransmission, e.vehicle.avgEmissionsPerKm, e.vehicle.rego), 
-          e.tripLegs.map((t) => new TripLeg(new Location(t.startLocation.lat, t.startLocation.long, t.startLocation.nickname),
-          new Location(t.endLocation.lat, t.endLocation.long, t.endLocation.nickname),t.duration, t.distance, 
-          new Vehicle(t.vehicle.make, t.vehicle.model, t.vehicle.year, t.vehicle.autoTransmission, t.vehicle.avgEmissionsPerKm, t.vehicle.rego)))))
-        
-      }
-  };  
+          ],
+        [0, 0, 0, 0]
+      );
+    }
+  }
+  objectToInstance(obj, dbKey) {
+    this.dbKey = dbKey;
+    this.consecutiveDuration = obj.consecutiveDuration;
+    this.date = new Date(Date.parse(obj.date));
+    this.distance = obj.distance;
+    this.emissions = obj.emissions;
+    this.totalDuration = obj.totalDuration;
+    this.vehicleTrips = obj.vehicleTrips.map((e) => new VehicleTrip(
+      new Vehicle(e.vehicle.make, e.vehicle.model, e.vehicle.year, e.vehicle.autoTransmission, e.vehicle.avgEmissionsPerKm, e.vehicle.rego),
+      e.tripLegs.map((t) => new TripLeg(new Location(t.startLocation.lat, t.startLocation.long, t.startLocation.nickname),
+        new Location(t.endLocation.lat, t.endLocation.long, t.endLocation.nickname), t.duration, t.distance,
+        new Vehicle(t.vehicle.make, t.vehicle.model, t.vehicle.year, t.vehicle.autoTransmission, t.vehicle.avgEmissionsPerKm, t.vehicle.rego)))))
+
+  }
+};
 
 const exampleCar = new Vehicle("Toyota", "Hilux", "2002", true, 13, "ABC123");
 const exampleCar2 = new Vehicle("Mazda", "6", "2012", true, 10, "KJC836");
@@ -96,14 +97,14 @@ const exampleLocation1 = new Location(-37.93556359903012, 145.0612926526105, "De
 const exampleLocation2 = new Location(-37.88547095750236, 145.08436342596195, "Coles");
 const exampleLocation3 = new Location(-37.94903921470658, 145.0406876435942, "Woolworths");
 const exampleTripPast = new Trip(new Date('8/2/2021'), [
-    new VehicleTrip(exampleCar, [
-      new TripLeg(exampleLocation1, exampleLocation2, 56, 72, exampleCar),
-      new TripLeg(exampleLocation2, exampleLocation1, 56, 72, exampleCar)
-    ]),
-    new VehicleTrip(exampleCar2, [
-      new TripLeg(exampleLocation1, exampleLocation3, 56, 72, exampleCar2),
-      new TripLeg(exampleLocation3, exampleLocation1, 56, 72, exampleCar2)
-    ])
+  new VehicleTrip(exampleCar, [
+    new TripLeg(exampleLocation1, exampleLocation2, 56, 72, exampleCar),
+    new TripLeg(exampleLocation2, exampleLocation1, 56, 72, exampleCar)
+  ]),
+  new VehicleTrip(exampleCar2, [
+    new TripLeg(exampleLocation1, exampleLocation3, 56, 72, exampleCar2),
+    new TripLeg(exampleLocation3, exampleLocation1, 56, 72, exampleCar2)
+  ])
 ]);
 const exampleTripCurrent = new Trip(new Date('8/3/2021'), [
   new VehicleTrip(exampleCar, [
