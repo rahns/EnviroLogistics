65f085f5b6d8196967ff84b84e68f5e8e7f2f764
Rahn Stavar <rsta0012@student.monash.edu>
2021-08-31 12:29:22 +1000

    Fixed missing solver package

---
+++ public/vrp-solver/vrp_cli_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function get_routing_locations(a: number): number;
+export function validate_pragmatic(a: number, b: number): number;
+export function convert_to_pragmatic(a: number, b: number, c: number): number;
+export function solve_pragmatic(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
